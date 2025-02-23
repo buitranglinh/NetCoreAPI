decd4b7c9d0198d6d959f4e89677c577ab5f64a8
TrangLinh <https://github.com/buitranglinh/PTPMQL.git>
2025-02-23 14:39:20 +0700

    BTH_PTPMQL

---
+++ MvcMovie/Models/Score.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+namespace MvcMovie.Models
+{
+    public class Score 
+    {
+        public double ScoreA { get; set; }
+        public double ScoreB { get; set;}
+        public double ScoreC { get; set;}
+    }
+}
