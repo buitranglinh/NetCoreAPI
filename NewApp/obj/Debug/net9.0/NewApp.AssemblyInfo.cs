c783a053c87752f60edae42dec7f28ea3990d1e0
TrangLinh <https://github.com/buitranglinh/PTPMQL.git>
2025-02-23 15:06:45 +0700

    BTH 10/2

--- NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb3c74a54bad7cd6666c01cba402c8ed754fa501")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b1edd52cda23dad098e8255cdda3a4716901d7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
