a90daf6d27fc4966eac6f21ebeb77a16d651a577
TrangLinh <https://github.com/buitranglinh/PTPMQL.git>
2025-02-27 14:31:36 +0700

    BTH_PTPMQL

--- MvcMovie/Models/Bill.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-namespace MvcMovie.Models
-{
-    public class Bill{
-    public string ProduceName { get; set; }
-    public int Quantity { get; set; }
-    public double UnitPrice { get; set; }
-    }
-}
