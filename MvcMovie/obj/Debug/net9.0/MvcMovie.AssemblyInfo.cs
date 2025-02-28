6d11584cc10fd21ea60b92acfa9b392326aa2883
TrangLinh <https://github.com/buitranglinh/PTPMQL.git>
2025-02-28 14:34:27 +0700

    BTH 10/2

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f59d723e20c6aae8aae217cd95e08bcac97075b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f24c6381c4c17cb915176c4806f8a146b082bdaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
